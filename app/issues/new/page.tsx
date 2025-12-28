8d721043a409c0880dbe3032e2adbf48065a2b7d
Jesus Gonzalez <reychucho859@gmail.com>
2025-12-28 18:26:54 -0500

    Implemented a Spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import axios from 'axios'
 import { validationSchema } from '@/app/validationSchema'
-import { TextField, TextArea, Button, Callout, Text } from '@radix-ui/themes'
+import { TextField, TextArea, Button, Callout, Spinner } from '@radix-ui/themes'
 import { InfoCircledIcon } from '@radix-ui/react-icons'
 import { useForm, Controller } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
@@ -10,6 +10,7 @@ import { useState } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import ErrorMessage from '@/app/components/ErrorMessage'
+import { is } from 'zod/v4/locales'
 
 type IssueForm = z.infer<typeof validationSchema>;
 
@@ -19,6 +20,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(validationSchema)
   });
   const [error, setError] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
 
   return (
     <div className='max-w-xl'>
@@ -36,9 +38,11 @@ const NewIssuePage = () => {
         className='space-y-3'
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true);
             await axios.post('/api/issues', data);
             router.push('/issues');
           } catch (error) {
+            setSubmitting(false);
             setError('An unexpected error occured');
           }
         })}
@@ -52,7 +56,7 @@ const NewIssuePage = () => {
         <ErrorMessage>
           {errors.description?.message}
         </ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>{isSubmitting && <Spinner />}Submit New Issue</Button>
       </form>
     </div>
   );
