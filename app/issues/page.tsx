51f8f7bb14d06675e3369c3e2853e1ad6fea0f61
Jesus Gonzalez <reychucho859@gmail.com>
2025-12-28 12:09:45 -0500

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 import { Button } from '@radix-ui/themes'
+import { PlusCircledIcon } from '@radix-ui/react-icons'
 
 const IssuesPage = () => {
   return (
     <div>
-      <Button>New Issue</Button>
+      <Button>
+        <PlusCircledIcon height='16' width='16'/>
+        <Link href='/issues/new'>New Issue</Link>
+      </Button>
     </div>
   )
 }
