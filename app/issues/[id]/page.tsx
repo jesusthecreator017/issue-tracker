b482e5f115739727a2521a0eb4ff47a2442c9c64
Jesus Gonzalez <reychucho859@gmail.com>
2025-12-29 14:19:54 -0500

    Added Edit Button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,12 @@
 import { IssueStatusBadge } from '@/app/components';
 import { prisma } from '@/prisma/client';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import { Pencil2Icon } from '@radix-ui/react-icons';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
 import rehypeSanitize from 'rehype-sanitize';
+import Link from 'next/link';
+import { issue } from '@uiw/react-md-editor';
 
 interface Props {
     params: { id: string }
@@ -21,16 +24,24 @@ const IssueDetailPage = async ({ params }: Props) => {
     }
 
     return (
-        <div>
-            <Heading>{currIssue.title}</Heading>
-            <Flex gap='3' my='2'>
-                <IssueStatusBadge status={currIssue.status} />
-                <Text>{currIssue.createdAt.toDateString()}</Text>
-            </Flex>
-            <Card className='prose prose-slate dark:prose-invert mt-4'>
-                <ReactMarkdown rehypePlugins={[[rehypeSanitize]]}>{currIssue.description}</ReactMarkdown>
-            </Card>
-        </div>
+        <Grid columns={{ initial: '1', md: '2' }} gap='5'>
+            <Box>
+                <Heading>{currIssue.title}</Heading>
+                <Flex gap='3' my='2'>
+                    <IssueStatusBadge status={currIssue.status} />
+                    <Text>{currIssue.createdAt.toDateString()}</Text>
+                </Flex>
+                <Card className='prose prose-slate dark:prose-invert mt-4'>
+                    <ReactMarkdown rehypePlugins={[[rehypeSanitize]]}>{currIssue.description}</ReactMarkdown>
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href={`/issues/${currIssue.id}/edit`}>Edit Issue</Link>
+                </Button>
+            </Box>
+        </Grid>
     )
 }
 
