876ac1ddddcbcd2d01620eeba940434b85c302b1
Jesus Gonzalez <reychucho859@gmail.com>
2026-01-01 13:29:15 -0500

    Added Delete Functionality to the button

--- app/issues/[id]/delete/DeleteIssueButton.tsx
+++ app/issues/[id]/delete/DeleteIssueButton.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { AlertDialog, Button, Flex } from "@radix-ui/themes"
+import axios from "axios"
+import { useRouter } from "next/navigation"
 
-const DeleteIssueButton = ({ issueID } : { issueId: number }) => {
+const DeleteIssueButton = ({ issueId } : { issueId: number }) => {
+    const router = useRouter();
   return (
     <AlertDialog.Root>
         <AlertDialog.Trigger>
@@ -21,7 +24,15 @@ const DeleteIssueButton = ({ issueID } : { issueId: number }) => {
                     </Button>
                 </AlertDialog.Cancel>
                 <AlertDialog.Action>
-                    <Button variant="soft" color="red">
+                    <Button 
+                        variant="soft"
+                        color="red"
+                        onClick={ async () => {
+                            await axios.delete('/api/issues/' + issueId);
+                            router.push('/issues');
+                            router.refresh();
+                        }}
+                    >
                         Delete Issue
                     </Button>
                 </AlertDialog.Action>
