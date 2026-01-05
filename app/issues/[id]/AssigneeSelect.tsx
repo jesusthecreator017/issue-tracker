b8cfaf957dae65029df49d4c49c12ddf4ae6cfd0
Jesus Gonzalez <reychucho859@gmail.com>
2026-01-05 15:40:57 -0500

    Implemented Toast Notificaiton for Patch Errors

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,41 +3,47 @@ import { Select, Skeleton } from '@radix-ui/themes'
 import { Issue, User } from '@/generated/prisma/client';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     const { data: users, error, isLoading } = useQuery<User[]>({
         queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then( res => res.data),
+        queryFn: () => axios.get('/api/users').then(res => res.data),
         staleTime: 60 * 1000, // 60s,
         retry: 3,
     });
 
-    if(isLoading) return <Skeleton height='1.5rem'/>
+    if (isLoading) return <Skeleton height='1.5rem' />
 
-    if(error) return null;
+    if (error) return null;
 
     return (
-        <Select.Root
-            defaultValue={issue.assignedToUserId || "unassigned"}
-            onValueChange={(userId) => {
-                axios.patch('/api/issues/' + issue.id, {
-                    assignedToUserId: userId === "unassigned" ? null : userId
-                });
-            }}
-        >
-            <Select.Trigger placeholder='Assign...' />
-            <Select.Content>
-                <Select.Group>
-                    <Select.Label>Suggestions</Select.Label>
-                    <Select.Item value="unassigned">Unassigned</Select.Item>
-                    {
-                        users?.map( user => (
-                            <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
-                        ))
-                    }
-                </Select.Group>
-            </Select.Content>
-        </Select.Root>
+        <>
+            <Select.Root
+                defaultValue={issue.assignedToUserId || "unassigned"}
+                onValueChange={(userId) => {
+                    axios.patch('/api/issues/' + issue.id, {
+                        assignedToUserId: userId === "unassigned" ? null : userId
+                    })
+                    .catch(() => {
+                        toast.error('Changes could not be saved.')
+                    });
+                }}
+            >
+                <Select.Trigger placeholder='Assign...' />
+                <Select.Content>
+                    <Select.Group>
+                        <Select.Label>Suggestions</Select.Label>
+                        <Select.Item value="unassigned">Unassigned</Select.Item>
+                        {
+                            users?.map(user => (
+                                <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+                            ))
+                        }
+                    </Select.Group>
+                </Select.Content>
+            </Select.Root>
+        </>
     )
 }
 
